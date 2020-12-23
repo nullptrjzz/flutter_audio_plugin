3ac30c358392b6e1fcbfc0ab3aeef4068b19795c
Jzz <jshazsj@qq.com>
2020-12-23 18:05:45 +0800

    feat(util): Add linux support
    Containing platform specified library, fonts and some performance improvement.
    
    [skip ci]

---
+++ example/linux/flutter/generated_plugin_registrant.cc
@@ -0,0 +1,13 @@
+//
+//  Generated file. Do not edit.
+//
+
+#include "generated_plugin_registrant.h"
+
+#include <flutter_audio_plugin/flutter_audio_plugin.h>
+
+void fl_register_plugins(FlPluginRegistry* registry) {
+  g_autoptr(FlPluginRegistrar) flutter_audio_plugin_registrar =
+      fl_plugin_registry_get_registrar_for_plugin(registry, "FlutterAudioPlugin");
+  flutter_audio_plugin_register_with_registrar(flutter_audio_plugin_registrar);
+}
